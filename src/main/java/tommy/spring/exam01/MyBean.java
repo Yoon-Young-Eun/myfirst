1851c4dc0562e8991475db74635d0d52041a4737
Yoon-Young-Eun <disdiddms@gmail.com>
2022-07-24 22:43:21 +0900

    2022-07-24

---
+++ src/main/java/tommy/spring/exam01/MyBean.java
@@ -0,0 +1,7 @@
+package tommy.spring.exam01;
+
+public class MyBean {
+  public void sayHellow(String name) {
+	   System.out.println("Hellow, " + name+ " !!!");
+  }
+}
---
+++ src/main/java/tommy/spring/exam02/HelloSpring.java
@@ -0,0 +1,13 @@
+package tommy.spring.exam02;
+
+import tommy.spring.exam01.MyBean;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+	   MyBean bean = new MyBean();  // ´ÙÇü¼º
+	   bean.sayHellow("Spring");
+
+	}
+
+}
